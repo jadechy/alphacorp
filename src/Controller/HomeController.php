e59c9c3ebbcce59dcce5a35120ae0ddc01969740
cecilelecerf <c.lecerf.cecile@gmail.com>
2025-01-25 11:41:15 +0100

    init

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends AbstractController
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('home/index.html.twig', [
+            "isDark" => true
         ]);
     }
 }
